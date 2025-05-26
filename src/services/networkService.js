8602835684cd97de5e85db0c76dc2142aed73358
ttosi <ttosi519@gmail.com>
2025-05-25 21:21:35 -0700

    implement network service, productDetails

--- src/services/networkService.js
+++ src/services/networkService.js
@@ -1,21 +1,14 @@
-import { useFetch, createFetch } from '@vueuse/core';
+import { createFetch } from '@vueuse/core';
 
 const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
-const headers = {
-  'Content-Type': 'application/json',
-  Authorization: API_TOKEN,
-};
 
-const useMyFetch = createFetch({
-  baseUrl: 'https://my-api.com',
+const useSecureFetch = createFetch({
   options: {
     async beforeFetch({ options }) {
       options.headers.Authorization = `Bearer ${API_TOKEN}`;
-      // options.headers.Authorization = `Bearer whatthefuck`;
-      options.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
       options.headers['Content-Type'] = 'application/json';
-      console.log('here we gopppoo', options);
+      options.headers['Access-Control-Allow-Methods'] = 'GET, POST';
       return { options };
     },
   },
@@ -25,11 +18,10 @@ const useMyFetch = createFetch({
 });
 
 const get = async (endpoint) => {
-  console.log('fetching');
-  const { data } = await useMyFetch(`${baseUrl}/${endpoint}`);
-  console.log(data.value);
+  const { data } = await useSecureFetch(`${baseUrl}/${endpoint}`);
+  console.debug(data.value);
 };
 
 const post = async () => {};
 
-export { get };
+export { get, post };
--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div>details</div>
 </template>
 
 <script setup></script>
