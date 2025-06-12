17b183fb3b09f879eb37667ebd7ca3f4b005310b
ttosi <ttosi519@gmail.com>
2025-06-12 12:22:41 -0700

    change dist output path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
@@ -11,4 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  root: './',
+  build: {
+    outDir: '../temp-deploy/dist',
+    emptyOutDir: true,
+  },
+});
